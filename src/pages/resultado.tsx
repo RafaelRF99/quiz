b7e4ccd925890bc267f771aea77ebdc962779129
Rafael Ferreira <rafaelfferreira99@hotmail.com>
2023-11-21 12:54:04 -0300

    feat: informações resultado

--- src/pages/resultado.tsx
+++ src/pages/resultado.tsx
@@ -1,7 +1,20 @@
-export default function resultado() {
+import styles from '../styles/Resultado.module.css'
+
+import { useRouter } from "next/router"
+
+export default function Resultado() {
+  const router = useRouter()
+
+  const total = router.query.total ? +router.query.total : 0
+  const certas = router.query.certas ? +router.query.certas : 0
+  const percentual = Math.round((certas / total) * 100)
+
   return (
-    <div>
+    <div className={styles.resultado}>
         <h1>Resultado</h1>
+        <div>Total: {total}</div>
+        <div>Certas: {certas}</div>
+        <div>{`${percentual}%`}</div>
     </div>
   )
 }
